0277f811b5d3bce189e1eadc9a16c2090760d066
Pelayo <pmartinezcapela@gmail.com>
2024-11-05 18:27:37 +0100

    Created env vbls

--- js/schedule/loadScheduler.mjs
+++ js/schedule/loadScheduler.mjs
@@ -1,97 +1,44 @@
-function convertDate(unixDate) {
-  var date = new Date(unixDate);
-  date.setHours(date.getHours() + 1);
-  var isoString = date.toISOString();
-  var split = isoString.split("T");
-  return split[0] + " " + split[1].split(".")[0];
-}
-
-function initialFilterDate() {
-  var date = new Date();
-  date.setHours(date.getHours() - 2);
-  return date.getTime();
-}
-
-function endingFilterDate() {
-  var date = new Date();
-  date.setHours(date.getHours() + 20);
-  console.log(date.getTime());
-
-  return date.getTime();
-}
-
-function crateElements(elements) {
-  let countMark = 1;
-  const table = document.createElement("table");
-  table.setAttribute("id", "modal-table");
-  const newHeaders = document.createElement("tr");
-  const titleDate = document.createElement("th");
-  titleDate.textContent = "FECHA";
-  const titleEvent = document.createElement("th");
-  titleEvent.textContent = "EVENTO";
-  newHeaders.appendChild(titleDate);
-  newHeaders.appendChild(titleEvent);
-  table.appendChild(newHeaders);
-  for (let i = 0; i < elements.length; i++) {
-    const newRow = document.createElement("tr");
-    const element = elements[i];
-    const dataDate = document.createElement("td");
-    dataDate.textContent = element.date;
-    dataDate.className = "event-date";
-    newRow.appendChild(dataDate);
-    const tdEvent = document.createElement("td");
-    const dataEvent = document.createElement("div");
-    const dataSubevent = document.createElement("div");
-    dataEvent.textContent = element.event;
-    dataEvent.className = "event-title";
-    dataSubevent.textContent = element.subtitle;
-    tdEvent.appendChild(dataEvent);
-    tdEvent.appendChild(dataSubevent);
-    newRow.appendChild(tdEvent);
-    if (countMark > 0 && element.ms > initialFilterDate()) {
-      newRow.className = "actual-event";
-      countMark--;
-    }
-    table.appendChild(newRow);
-  }
-  document.getElementById("modal-data").appendChild(table);
-}
-
-const url = "https://miguia.tv/v/";
-const proxy = "https://corsproxy.io/?";
 export async function obtainSchedule(extension) {
   const { openModal, closeModal } = await import("../modal/modalActions.mjs");
-  fetch(proxy + url + extension)
-    .then((response) => response.text())
-    .then((html) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-      const data = Array.from(doc.querySelectorAll(".media-body"))
-        .map((el) => {
-          const children = el.children;
-          const hour = parseInt(children[0].children[1].children[1].getAttribute("data-unixtime")) * 1000;
-          const eventTitle = children[1].innerText;
-          const eventSubtitle = children[2].innerHTML;
-          const length = parseInt(children[0].children[2].children[0].innerText.split(" ")[1]);
-          return {
-            ms: hour,
-            date: `${convertDate(hour)}`,
-            event: `${eventTitle}`,
-            subtitle: `(${length}min) ${eventSubtitle}`,
-            length: length,
-          };
+  const { convertDate, initialFilterDate, endingFilterDate, crateElements } = await import("./loadUtils.mjs");
+  fetch("config.json")
+    .then((res) => res.json())
+    .then((conf) => {
+      const { URL_SCHEDULE, PROXY_SCHEDULE } = conf;
+      fetch(PROXY_SCHEDULE + URL_SCHEDULE + extension)
+        .then((response) => response.text())
+        .then((html) => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+          const data = Array.from(doc.querySelectorAll(".media-body"))
+            .map((el) => {
+              const children = el.children;
+              const hour = parseInt(children[0].children[1].children[1].getAttribute("data-unixtime")) * 1000;
+              const eventTitle = children[1].innerText;
+              const eventSubtitle = children[2].innerHTML;
+              const length = parseInt(children[0].children[2].children[0].innerText.split(" ")[1]);
+              console.log(eventSubtitle);
+
+              return {
+                ms: hour,
+                date: `${convertDate(hour)}`,
+                event: `${eventTitle}`,
+                subtitle: `(${length}min) ${eventSubtitle}`,
+                length: length,
+              };
+            })
+            .filter((el) => el.ms > initialFilterDate())
+            .filter((el) => el.ms < endingFilterDate());
+          if (data.length === 0) throw new Error("Sin datos que cargar");
+          crateElements(data);
         })
-        .filter((el) => el.ms > initialFilterDate())
-        .filter((el) => el.ms < endingFilterDate());
-      if (data.length === 0) throw new Error("Error cargando datos");
-      crateElements(data);
-    })
-    .then(() => {
-      openModal();
-    })
-    .catch((error) => {
-      closeModal();
-      console.error("Error:", error);
-      alert(error.message);
+        .then(() => {
+          openModal();
+        })
+        .catch((error) => {
+          closeModal();
+          console.error("Error:", error);
+          alert(error.message);
+        });
     });
 }
---
+++ js/schedule/loadUtils.mjs
@@ -0,0 +1,56 @@
+export function convertDate(unixDate) {
+  var date = new Date(unixDate);
+  date.setHours(date.getHours() + 1);
+  var isoString = date.toISOString();
+  var split = isoString.split("T");
+  return split[0] + " " + split[1].split(".")[0];
+}
+
+export function initialFilterDate() {
+  var date = new Date();
+  date.setHours(date.getHours() - 2);
+  return date.getTime();
+}
+
+export function endingFilterDate() {
+  var date = new Date();
+  date.setHours(date.getHours() + 20);
+  return date.getTime();
+}
+
+export function crateElements(elements) {
+  let countMark = 1;
+  const table = document.createElement("table");
+  table.setAttribute("id", "modal-table");
+  const newHeaders = document.createElement("tr");
+  const titleDate = document.createElement("th");
+  titleDate.textContent = "FECHA";
+  const titleEvent = document.createElement("th");
+  titleEvent.textContent = "EVENTO";
+  newHeaders.appendChild(titleDate);
+  newHeaders.appendChild(titleEvent);
+  table.appendChild(newHeaders);
+  for (let i = 0; i < elements.length; i++) {
+    const newRow = document.createElement("tr");
+    const element = elements[i];
+    const dataDate = document.createElement("td");
+    dataDate.textContent = element.date;
+    dataDate.className = "event-date";
+    newRow.appendChild(dataDate);
+    const tdEvent = document.createElement("td");
+    const dataEvent = document.createElement("div");
+    const dataSubevent = document.createElement("div");
+    dataEvent.textContent = element.event;
+    dataEvent.className = "event-title";
+    dataSubevent.textContent = element.subtitle;
+    tdEvent.appendChild(dataEvent);
+    tdEvent.appendChild(dataSubevent);
+    newRow.appendChild(tdEvent);
+    if (countMark > 0 && element.ms > initialFilterDate()) {
+      newRow.className = "actual-event";
+      countMark--;
+    }
+    table.appendChild(newRow);
+  }
+  document.getElementById("modal-data").appendChild(table);
+}
